3b37830cc4ee424a51fb0ed30e51956e8abceb09
zMag33z <mag33.cdh@gmail.com>
2023-05-07 12:26:49 -0500

    initial commit

---
+++ demotech-news-java-api/src/main/java/com/technews/DemotechNewsJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotechNewsJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotechNewsJavaApiApplication.class, args);
+	}
+
+}

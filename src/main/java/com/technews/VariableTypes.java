5eabcfca936281cefd35612d3e321ed180979c96
zMag33z <mag33.cdh@gmail.com>
2023-05-08 20:50:06 -0500

    created classes and overrode hashing and default data functionality to receive from parent class

--- src/main/java/com/technews/VariableTypes.java
+++ src/main/java/com/technews/VariableTypes.java
@@ -1,7 +1,7 @@
 package com.technews;
 
-public class HelloWorld {
+public class VariableTypes {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+
     }
 }
